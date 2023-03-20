eb6cdd1f4b5eba3262114c90a8af775938c95d2b
Simon Bell <bimonsell@gmail.com>
2023-03-21 00:01:13 +0400

    Replacement works, project restructured

--- converter.js
+++ converter.js
@@ -1,36 +1,10 @@
-'use strict';
-
-let initial, replaceWith, replaceInput, withInput;
+let allItems, initial, replaceWith;
 
 chrome.storage.sync.get(null, function (items) {
   allItems = Object.entries(items);
-
-  if (allItems.length == 0) {
-    initial = [];
-    replaceWith = [];
-  } else {
-    initial = allItems[0][1];
-    replaceWith = allItems[1][1];
-  }
+  initial = allItems[0][1];
+  replaceWith = allItems[1][1];
 });
-
-try {
-  document
-    .getElementById('the-form')
-    .addEventListener('submit', function (event) {
-      // event.preventDefault();
-      replaceInput = document.querySelector('#replace').value;
-      withInput = document.querySelector('#with').value;
-      initial.push(replaceInput);
-      replaceWith.push(withInput);
-      chrome.storage.sync.set({ initial: initial, replaceWith: replaceWith });
-      console.log(
-        `userSpecified is updated! Words being replaced: ${initial}. Substitutions:  ${replaceWith}`
-      );
-    });
-} catch (error) {
-  console.log(error);
-}
 
 const converter = (element) => {
   if (element.hasChildNodes()) {
@@ -38,7 +12,7 @@ const converter = (element) => {
   }
   if ((element.nodeType === Text.TEXT_NODE) & (initial.length > 0)) {
     for (let i = 0; i < initial.length; i++) {
-      rgx = new RegExp(`${initial[i]}`);
+      rgx = new RegExp(`${initial[i]}`, 'gmi');
       if (element.textContent.match(rgx)) {
         element.textContent = element.textContent.replace(
           initial[i],
@@ -66,5 +40,4 @@ setTimeout(() => {
   converter(document.head);
   converter(document.body);
   refresher.observe(document.body, { childList: true, subtree: true });
-  console.log(initial, replaceWith);
-}, 50);
+}, 150);
---
+++ popup.js
@@ -0,0 +1,47 @@
+let allItems,
+  keyItems,
+  valueItems,
+  initial,
+  replaceWith,
+  replaceInput,
+  withInput;
+
+// One function to sync words
+function wordsSync() {
+  chrome.storage.sync.set({
+    initial: initial,
+    replaceWith: replaceWith,
+  });
+}
+
+// Creating entries in the storage on the first run
+chrome.storage.sync.get(null, function (items) {
+  allItems = Object.entries(items);
+  keyItems = Object.keys(items);
+  valueItems = Object.values(items);
+
+  if (!keyItems.includes('initial')) {
+    initial = [];
+    replaceWith = [];
+    wordsSync();
+    console.log('Has been launched for the first time');
+  } else {
+    initial = allItems[0][1];
+    replaceWith = allItems[1][1];
+    wordsSync();
+    console.log('Has been launched NOT for the first time');
+  }
+});
+
+setTimeout(() => {
+  document
+    .getElementById('the-form')
+    .addEventListener('submit', function (event) {
+      // event.preventDefault();
+      replaceInput = document.querySelector('#replace').value;
+      withInput = document.querySelector('#with').value;
+      initial.push(replaceInput);
+      replaceWith.push(withInput);
+      wordsSync();
+    });
+}, 100);
