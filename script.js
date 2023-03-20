2730b0b06ee1bcce0333efdbb698c76584be9a40
Simon Bell <bimonsell@gmail.com>
2023-03-20 14:11:45 +0400

    Fixed Regex and basic func

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+'use strict';
+
 let initial, replaceWith, replaceInput, withInput;
 
 chrome.storage.sync.get(null, function (items) {
   allItems = Object.entries(items);
 
-  if (allItems.length > 0) {
-    initial = allItems[0][1];
-    replaceWith = allItems[1][1];
-  } else {
+  if (allItems.length == 0) {
     initial = [];
     replaceWith = [];
+  } else {
+    initial = allItems[0][1];
+    replaceWith = allItems[1][1];
   }
 });
 
-document
-  .getElementById('the-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-    replaceInput = document.querySelector('#replace').value;
-    withInput = document.querySelector('#with').value;
-    initial.push(replaceInput);
-    replaceWith.push(withInput);
-    chrome.storage.sync.set({ initial: initial });
-    chrome.storage.sync.set({ replaceWith: replaceWith });
-    console.log(
-      `userSpecified is updated! Words being replaced: ${initial}. Substitutions:  ${replaceWith}`
-    );
-  });
+try {
+  document
+    .getElementById('the-form')
+    .addEventListener('submit', function (event) {
+      // event.preventDefault();
+      replaceInput = document.querySelector('#replace').value;
+      withInput = document.querySelector('#with').value;
+      initial.push(replaceInput);
+      replaceWith.push(withInput);
+      chrome.storage.sync.set({ initial: initial, replaceWith: replaceWith });
+      console.log(
+        `userSpecified is updated! Words being replaced: ${initial}. Substitutions:  ${replaceWith}`
+      );
+    });
+} catch (error) {
+  console.log(error);
+}
 
 const converter = (element) => {
   if (element.hasChildNodes()) {
@@ -33,7 +38,8 @@ const converter = (element) => {
   }
   if ((element.nodeType === Text.TEXT_NODE) & (initial.length > 0)) {
     for (let i = 0; i < initial.length; i++) {
-      if (element.textContent.match(initial[i])) {
+      rgx = new RegExp(`${initial[i]}`);
+      if (element.textContent.match(rgx)) {
         element.textContent = element.textContent.replace(
           initial[i],
           replaceWith[i]
@@ -60,5 +66,5 @@ setTimeout(() => {
   converter(document.head);
   converter(document.body);
   refresher.observe(document.body, { childList: true, subtree: true });
-  console.log(initial);
-}, 100);
+  console.log(initial, replaceWith);
+}, 50);
